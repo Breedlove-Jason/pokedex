c433a06a2016214965aa1c95e7c9abe9f6b7baac
Jason Breedlove <j@jasonbreedlove.com>
2022-06-26 14:28:14 -0700

    finished styling
    project moved to QA

--- src/components/Pokecard/Pokecard.component.js
+++ src/components/Pokecard/Pokecard.component.js
@@ -2,8 +2,6 @@ import React from "react";
 import "./Pokecard.styles.css";
 
 function Pokecard(props) {
-  // const POKE_API =
-  //   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
   const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
 
   let padToThree = (number) =>
@@ -13,7 +11,9 @@ function Pokecard(props) {
   return (
     <div className="Pokecard">
       <h1 className={"Pokecard-title"}>{props.name}</h1>
-      <img src={pokeImg} alt={props.name} />
+      <div className="Pokecard-image">
+        <img src={pokeImg} alt={props.name} />
+      </div>
       <h1 className={"Pokecard-data"}>{props.type}</h1>
       <h1 className={"Pokecard-data"}>{props.exp}</h1>
     </div>
